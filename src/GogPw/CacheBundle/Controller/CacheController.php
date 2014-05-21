04ae282a28fc8fd107f6d8ea763eb22f5353b297
www-data <www-data@webserver>
2014-05-21 10:02:37 +0200

    Add memcache support.

--- src/GogPw/CacheBundle/Controller/CacheController.php
+++ src/GogPw/CacheBundle/Controller/CacheController.php
@@ -9,11 +9,32 @@ class CacheController extends Controller
 {
 
     const SLEEP_TIME = 3;
+    const MEMCACHE_TTL = 5;
 
     public function nocacheAction()
     {
         $response = $this->getResponse();
         return new Response($response);
+    }
+
+    public function memcacheAction()
+    {
+        $response = $this->getResponseFromMemcache();
+        return new Response($response);
+    }
+
+    private function getResponseFromMemcache()
+    {
+        $cacheService = $this->container->get('memcache.default');
+        if(!$response = $cacheService->get('memcache_key')) {
+            $response = $this->getResponse();
+            syslog(5, 'Caching response using memcache.');
+            $cacheService->set('memcache_key', $response, self::MEMCACHE_TTL);   
+        } else {
+            syslog(5, 'Fetched response using memcache.');
+        }
+    
+        return $response;
     }
 
     private function getResponse()
